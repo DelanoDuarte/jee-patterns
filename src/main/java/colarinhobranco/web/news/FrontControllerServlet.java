c86301cd8b95c3e426120ed764507d5536991e83
DelanoJunior <delano.duarte@gmail.com>
2017-05-06 13:10:59 -0300

    Tentando Implementar o padrão Front Controller !

--- src/main/java/colarinhobranco/web/news/FrontControllerServlet.java
+++ src/main/java/colarinhobranco/web/news/FrontControllerServlet.java
@@ -2,43 +2,28 @@ package colarinhobranco.web.news;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import colarinhobranco.commands.CommandFrontController;
-
 /**
  * Servlet implementation class FrontControllerServlet
  */
-@WebServlet("/*")
+@WebServlet("*")
 public class FrontControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	CommandFrontController commandFrontController = null;
-
 	public FrontControllerServlet() {
 		super();
-	}
-
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-			throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-
-		commandFrontController = (CommandFrontController) Class
-				.forName("colarinhobranco.web.news." + request.getParameter("command")).newInstance();
-
-		commandFrontController.dispatcher(request, response);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		try {
-
-			String page = request.getRequestURI();
-			System.out.println(page);
 
 			processRequest(request, response);
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
@@ -51,6 +36,22 @@ public class FrontControllerServlet extends HttpServlet {
 		try {
 			processRequest(request, response);
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+
+		String nextPage = request.getPathInfo();
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+
+		try {
+			dispatcher.forward(request, response);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
