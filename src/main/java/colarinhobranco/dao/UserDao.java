a899ef03976f8118b1f226c28c910274ef065c89
lincolwn <lincolwn@gmail.com>
2017-05-10 09:06:21 -0300

    servlet e pagina de login

--- src/main/java/colarinhobranco/dao/UserDao.java
+++ src/main/java/colarinhobranco/dao/UserDao.java
@@ -6,9 +6,11 @@ import colarinhobranco.model.User;
 
 public interface UserDao {
 
-public User save(User user);
+	public User save(User user);
 	
 	public User get(Integer id);
+	
+	public User get(String login);
 	
 	public List<User> findAll();
 
