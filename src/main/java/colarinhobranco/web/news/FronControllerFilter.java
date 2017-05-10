6cec2022ec852370c84ff0386de45400ffed8054
lincolwn <lincolwn@gmail.com>
2017-05-10 09:21:45 -0300

    intercepting filter

--- src/main/java/colarinhobranco/web/news/FronControllerFilter.java
+++ src/main/java/colarinhobranco/web/news/FronControllerFilter.java
@@ -1,6 +1,7 @@
 package colarinhobranco.web.news;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,6 +12,14 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import colarinhobranco.dao.UserDao;
+import colarinhobranco.daoimpl.UserDaoImpl;
+import colarinhobranco.model.User;
+
+import javax.servlet.http.Cookie;
 
 /**
  * Servlet Filter implementation class FronControllerFilter
@@ -18,6 +27,10 @@ import javax.servlet.http.HttpServletRequest;
 @WebFilter(filterName = "FrontControllerFilter", urlPatterns = { "/pages/*", "/*" })
 public class FronControllerFilter implements Filter {
 
+	private final String TOKEN = "TOKEN";
+	private final String loginPath = "/login";
+
+	
 	/**
 	 * Default constructor.
 	 */
@@ -38,10 +51,22 @@ public class FronControllerFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
-		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		String path = httpServletRequest.getServletPath();
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
-		requestDispatcher.forward(request, response);
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		
+		String path = req.getServletPath();
+		
+		if (!checkToken(req) || validaToken(req, req.getRequestedSessionId()))
+		{
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher(loginPath);
+			requestDispatcher.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+			requestDispatcher.forward(request, response);
+		}
 	}
 
 	/**
@@ -49,6 +74,46 @@ public class FronControllerFilter implements Filter {
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
 		// TODO Auto-generated method stub
+		
+		UserDaoImpl dao = new UserDaoImpl();
+		//List<User> users = dao.findAll();
+		//if (users.size() == 0)
+		//{
+			User user = new User();
+			user.setLogin("user");
+			user.setSenha("1234");
+			dao.save(user);
+		//}
+		
+	}
+	
+	
+	private boolean checkToken(HttpServletRequest req)
+	{
+		boolean hasToken = false;
+		
+		for (Cookie c : req.getCookies())
+		{
+			hasToken = c.getName().equals(TOKEN) ? true : false;
+			System.out.println("nome " + c.getName() + ", valor: " + c.getValue());
+			if (hasToken)
+				break;
+		}
+		
+		return hasToken;
+	}
+	
+	private boolean validaToken(HttpServletRequest req, String valor)
+	{	
+		boolean tokenOk = false;
+		
+		for (Cookie c : req.getCookies())
+		{
+			tokenOk = c.getName().equals(req.getRequestedSessionId()) ? true : false;
+			if (tokenOk)
+				break;
+		}
+		return tokenOk;
 	}
 
 }
